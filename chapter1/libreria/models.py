80a0ad581f91170c52ef8105d8bc1153973710e6
nfmmendes <nilson.fmmendes@outlook.com>
2025-06-17 22:20:28 +0200

    Create models for libreria

--- chapter1/libreria/models.py
+++ chapter1/libreria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Autore(models.Model):
+    nome = models.CharField(max_length = 50)
+    cognome = models.CharField(max_length = 50)
+
+class Genere(models.Model):
+    descrizione = models.CharField(max_length = 30)
+
+class Libro(models.Model):
+    titolo = models.CharField(max_length = 200)
+    autore = models.ForeignKey(Autore, on_delete = models.PROTECT)
+    genere = models.ForeignKey(Genere, on_delete = models.PROTECT)
+
