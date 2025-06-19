5e17b9665efdfea2f12ae7859ce1d5458d7b13de
nfmmendes <nilson.fmmendes@outlook.com>
2025-06-19 23:11:05 +0200

    Register models on admin file

--- chapter1/libreria/admin.py
+++ chapter1/libreria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Genere)
+admin.site.register(models.Autore)
+admin.site.register(models.Libro)
+
+
