a421c36caeaeb6e188cc1eb2cf06eb0db13cce4c
nfmmendes <nilson.fmmendes@outlook.com>
2025-06-19 23:12:20 +0200

    Add class meta inside each model class

--- chapter1/libreria/models.py
+++ chapter1/libreria/models.py
@@ -6,11 +6,20 @@ class Autore(models.Model):
     nome = models.CharField(max_length = 50)
     cognome = models.CharField(max_length = 50)
 
+    class Meta:
+        verbose_name_plural = "Autori"
+
 class Genere(models.Model):
     descrizione = models.CharField(max_length = 30)
+
+    class Meta:
+        verbose_name_plural = "Generi"
 
 class Libro(models.Model):
     titolo = models.CharField(max_length = 200)
     autore = models.ForeignKey(Autore, on_delete = models.PROTECT)
     genere = models.ForeignKey(Genere, on_delete = models.PROTECT)
+
+    class Meta:
+        verbose_name_plural = "Libri"
 
