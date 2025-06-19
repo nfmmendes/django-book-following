10b1c57f1c6ac06ce59b5fba0bcd7a7d69d8fda1
nfmmendes <nilson.fmmendes@outlook.com>
2025-06-19 23:19:55 +0200

    Add __str__ functions to models

--- chapter1/libreria/models.py
+++ chapter1/libreria/models.py
@@ -6,11 +6,17 @@ class Autore(models.Model):
     nome = models.CharField(max_length = 50)
     cognome = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return f"{self.nome} {self.cognome}" 
+
     class Meta:
         verbose_name_plural = "Autori"
 
 class Genere(models.Model):
     descrizione = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return f"{self.descrizione}"
 
     class Meta:
         verbose_name_plural = "Generi"
@@ -19,6 +25,9 @@ class Libro(models.Model):
     titolo = models.CharField(max_length = 200)
     autore = models.ForeignKey(Autore, on_delete = models.PROTECT)
     genere = models.ForeignKey(Genere, on_delete = models.PROTECT)
+
+    def __str__(self):
+        return f"{self.titolo}"
 
     class Meta:
         verbose_name_plural = "Libri"
